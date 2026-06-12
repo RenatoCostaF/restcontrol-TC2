cfb4bdd7ede071f9b16823960e7fa9300850ffc3
Renato Furtado <renatofurtado33@gmail.com>
2026-06-11 22:30:13 -0300

    first commit

---
+++ src/test/java/com/restcontrol/restcontrol_TC2/RestcontrolTc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restcontrol.restcontrol_TC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcontrolTc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
