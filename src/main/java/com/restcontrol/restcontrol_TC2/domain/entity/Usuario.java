9b5c1cffd473f81b60d12317784db8a84c82985f
Rafa130206 <rafabompadre@gmail.com>
2026-06-11 23:21:57 -0300

    Entidade Usuario criada

--- src/main/java/com/restcontrol/restcontrol_TC2/domain/entity/Usuario.java
+++ src/main/java/com/restcontrol/restcontrol_TC2/domain/entity/Usuario.java
@@ -1,4 +1,92 @@
 package com.restcontrol.restcontrol_TC2.domain.entity;
 
+import java.util.UUID;
+
 public class Usuario {
+    private UUID id;
+    private String nome;
+    private String email;
+    private String senha;
+    private TipoUsuario tipoUsuarioId;
+
+    private static void emailValido(String email) {
+        if (email == null || !email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            throw new IllegalArgumentException("Email inválido.");
+        }
+    }
+
+    private static void senhaValida(String senha) {
+        if (senha == null || senha.length() < 8) {
+            throw new IllegalArgumentException("A senha deve conter pelo menos 8 caracteres.");
+        }
+    }
+
+    public static Usuario create(UUID id, String nome, String email, String senha, TipoUsuario tipoUsuarioId) throws IllegalArgumentException {
+        if (nome == null || tipoUsuarioId == null) {
+            throw new IllegalArgumentException("Faltam dados.");
+        }
+
+        emailValido(email);
+        senhaValida(senha);
+
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+        usuario.setTipoUsuarioId(tipoUsuarioId);
+
+        return usuario;
+    }
+
+    public Usuario() {
+    }
+
+    public Usuario(UUID id, String nome, String email, String senha, TipoUsuario tipoUsuarioId) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipoUsuarioId = tipoUsuarioId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public TipoUsuario getTipoUsuarioId() {
+        return tipoUsuarioId;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setTipoUsuarioId(TipoUsuario tipoUsuarioId) {
+        this.tipoUsuarioId = tipoUsuarioId;
+    }
 }
