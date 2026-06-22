409f1a7c2dca5cff81beabac0a5ef79c837a58a1
Rafa130206 <rafabompadre@gmail.com>
2026-06-22 12:34:19 -0300

    created menu item adapters

---
+++ src/main/java/com/restcontrol/restcontrol_TC2/domain/Adapter/MenuItem/Output/UpdateMenuItemOutput.java
@@ -0,0 +1,13 @@
+package com.restcontrol.restcontrol_TC2.domain.Adapter.MenuItem.Output;
+
+public record UpdateMenuItemOutput(
+        String id,
+        String name,
+        String description,
+        Double price,
+        Boolean availableForDelivery,
+        String imageUrl,
+        String restaurantId,
+        Boolean isActive
+) {
+}
