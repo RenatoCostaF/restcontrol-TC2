b83abbb451575f6e09ecd7ec2f186a74f73f34b9
Rafa130206 <rafabompadre@gmail.com>
2026-06-22 13:01:52 -0300

    created menu item document

---
+++ src/main/java/com/restcontrol/restcontrol_TC2/infra/persistence/mongo/entity/MenuItemDocument.java
@@ -0,0 +1,82 @@
+package com.restcontrol.restcontrol_TC2.infra.persistence.mongo.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "menu_items")
+public class MenuItemDocument {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean availableForDelivery;
+    private String imageUrl;
+    private String restaurantId;
+    private Boolean isActive;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailableForDelivery() {
+        return availableForDelivery;
+    }
+
+    public void setAvailableForDelivery(Boolean availableForDelivery) {
+        this.availableForDelivery = availableForDelivery;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
