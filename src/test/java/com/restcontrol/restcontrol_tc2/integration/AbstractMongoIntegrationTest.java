effbdfb0ad67aaf37d23758cc7424599a9cb9fe4
Rafa130206 <rafabompadre@gmail.com>
2026-07-07 19:16:41 -0300

    fix

--- src/test/java/com/restcontrol/restcontrol_tc2/integration/AbstractMongoIntegrationTest.java
+++ src/test/java/com/restcontrol/restcontrol_tc2/integration/AbstractMongoIntegrationTest.java
@@ -3,23 +3,29 @@ package com.restcontrol.restcontrol_tc2.integration;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
-@Testcontainers
 public abstract class AbstractMongoIntegrationTest {
 
-    @Container
-    @ServiceConnection
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:7.0"));
+    private static final MongoDBContainer MONGO_DB_CONTAINER;
+
+    static {
+        MONGO_DB_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:8.0"));
+        MONGO_DB_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void configureMongo(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", MONGO_DB_CONTAINER::getReplicaSetUrl);
+    }
 
     @Autowired
     protected MockMvc mockMvc;
