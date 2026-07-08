4bada7a8ee9e6c9b957b313ff91178bf2e3e3eaf
gabrielressa <gabriel.santos@snyk.io>
2026-07-08 10:47:49 -0300

    Restore AbstractMongoIntegrationTest

---
+++ src/test/java/com/restcontrol/restcontrol_tc2/integration/AbstractMongoIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.restcontrol.restcontrol_tc2.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+import tools.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+@Testcontainers(disabledWithoutDocker = true)
+public abstract class AbstractMongoIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    private static final MongoDBContainer MONGO_DB_CONTAINER =
+            new MongoDBContainer(DockerImageName.parse("mongo:8.0"));
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    void cleanDatabase() {
+        mongoTemplate.dropCollection("users");
+        mongoTemplate.dropCollection("restaurants");
+        mongoTemplate.dropCollection("menu_items");
+    }
+}
